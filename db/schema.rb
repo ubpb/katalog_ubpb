e42e0e6ae5af9eb4ea50fd85e7a37e91baa48e28
René Sprotte <rene.sprotte@provideal.net>
2015-09-23 17:21:32 +0200

    Rebuilt DB

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141006140435) do
 
-  create_table "katalog_user_notes", force: :cascade do |t|
+  create_table "skala_user_notes", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "record_id"
     t.string   "scope_id"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20141006140435) do
     t.datetime "updated_at"
   end
 
-  add_index "katalog_user_notes", ["record_id"], name: "index_katalog_user_notes_on_record_id"
-  add_index "katalog_user_notes", ["user_id"], name: "index_katalog_user_notes_on_user_id"
+  add_index "skala_user_notes", ["record_id"], name: "index_skala_user_notes_on_record_id"
+  add_index "skala_user_notes", ["user_id"], name: "index_skala_user_notes_on_user_id"
 
-  create_table "katalog_user_watch_list_entries", force: :cascade do |t|
+  create_table "skala_user_watch_list_entries", force: :cascade do |t|
     t.string   "record_id"
     t.string   "scope_id"
     t.integer  "watch_list_id"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20141006140435) do
     t.datetime "updated_at"
   end
 
-  add_index "katalog_user_watch_list_entries", ["watch_list_id"], name: "index_katalog_user_watch_list_entries_on_watch_list_id"
+  add_index "skala_user_watch_list_entries", ["watch_list_id"], name: "index_skala_user_watch_list_entries_on_watch_list_id"
 
-  create_table "katalog_user_watch_lists", force: :cascade do |t|
+  create_table "skala_user_watch_lists", force: :cascade do |t|
     t.text     "description"
     t.string   "name"
     t.boolean  "public",      default: false
@@ -44,17 +44,16 @@ ActiveRecord::Schema.define(version: 20141006140435) do
     t.datetime "updated_at"
   end
 
-  add_index "katalog_user_watch_lists", ["user_id"], name: "index_katalog_user_watch_lists_on_user_id"
+  add_index "skala_user_watch_lists", ["user_id"], name: "index_skala_user_watch_lists_on_user_id"
 
-  create_table "katalog_users", force: :cascade do |t|
+  create_table "skala_users", force: :cascade do |t|
     t.string   "api_key"
-    t.string   "scope_id"
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "katalog_users", ["api_key"], name: "index_katalog_users_on_api_key", unique: true
-  add_index "katalog_users", ["username"], name: "index_katalog_users_on_username"
+  add_index "skala_users", ["api_key"], name: "index_skala_users_on_api_key", unique: true
+  add_index "skala_users", ["username"], name: "index_skala_users_on_username", unique: true
 
 end
