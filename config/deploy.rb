a414f7bbac2fde48f6f909395fe0bdccd6552ad8
René Sprotte <rene.sprotte@provideal.net>
2015-10-13 11:48:28 +0200

    kalalog.yml -> skala.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch,      "master"
 set :log_level,   :debug
 
 set :linked_files, fetch(:linked_files, []).push(
-  "config/database.yml", "config/secrets.yml", "config/katalog.yml"
+  "config/database.yml", "config/secrets.yml", "config/skala.yml"
 )
 set :linked_dirs, fetch(:linked_dirs, []).push(
   "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle",
