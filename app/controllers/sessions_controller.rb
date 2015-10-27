2c825e5c330139a634812e909b60553c0582e673
René Sprotte <rene.sprotte@provideal.net>
2015-10-27 17:06:52 +0100

    WIP.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
+
   skip_before_action :add_breadcrumb, :only => [:new]
 
   def create
-    @authenticate_user = Skala::AuthenticateUserService.new(authenticate_user_params)
+    @authenticate_user = AuthenticateUserService.new(authenticate_user_params)
 
     if @authenticate_user.valid?
-      @authenticate_user.call.try do |call|
-        if call.succeeded?
-          if call.result == true
-            session[:user_id] = Skala::User.find_or_create_by(username: call.username.downcase) do |user|
+      @authenticate_user.call.try do |op|
+        if op.succeeded?
+          if op.result == true
+            session[:user_id] = User.find_or_create_by(ilsuserid: op.username.upcase) do |user|
               # set attribute here...
             end.id
 
@@ -33,7 +34,7 @@ class SessionsController < ApplicationController
   end
 
   def new
-    @authenticate_user = Skala::AuthenticateUserService.new
+    @authenticate_user = AuthenticateUserService.new
   end
 
 private
