8dff76bff18df0c4ec73a9a67133d0ab6458a7e8
René Sprotte <rene.sprotte@provideal.net>
2015-10-28 14:35:47 +0100

    Fix.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,5 +1,5 @@
 module KatalogUbpb
   def self.config
-    @config ||= Skala::Config.new(Rails.application.config_for(:config))
+    @config ||= Config.new(Rails.application.config_for(:config))
   end
 end
