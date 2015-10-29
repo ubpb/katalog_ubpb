4a92a6eeed2625fe707bcaee625c56fc988eda74
Michael Sievers <michael_sievers@web.de>
2015-10-29 15:50:11 +0100

    Removed app/assets from view paths

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ module KatalogUbpb
       Rails.root.join("app", "services", "concerns")
     ])
 
-    config.paths["app/views"] << "app/assets"
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "Berlin"
