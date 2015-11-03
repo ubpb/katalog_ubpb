c82bfab62d8e227535e1f213573b90952ada5361
Michael Sievers <michael_sievers@web.de>
2015-11-03 14:57:30 +0100

    Hold requests and loans

--- app/controllers/users/hold_requests_controller.rb
+++ app/controllers/users/hold_requests_controller.rb
@@ -5,10 +5,32 @@ class Users::HoldRequestsController < UsersController
   end, only: :index
 
   def index
-    @hold_requests = Skala::User::HoldRequestDecorator.decorate_collection Skala::GetUserHoldRequestsService.call({
-      ils_adapter: KatalogUbpb.config.ils_adapter.instance,
-      user_id: current_user.username
-    })
+    ils_adapter = KatalogUbpb.config.ils_adapter.instance
+    search_engine_adapter = KatalogUbpb.config.ils_adapter.scope.search_engine_adapter.instance
+    @scope = KatalogUbpb.config.ils_adapter.scope
+
+    @hold_requests = GetUserHoldRequestsService.call(
+      adapter: ils_adapter,
+      user: current_user
+    )
+
+    @search_result = SearchRecordsService.call(
+      adapter: search_engine_adapter,
+      search_request: @search_request = Skala::SearchRequest.new(
+        queries: [
+          {
+            type: "ordered_terms",
+            field: "id",
+            terms: @hold_requests.map(&:record_id)
+          }
+        ],
+        size: @hold_requests.length
+      )
+    )
+
+    @records_by_id = @search_result.hits.each_with_object({}) do |_hit, _hash|
+      _hash[_hit.fields["id"]] = _hit
+    end
   end
 
   def destroy
