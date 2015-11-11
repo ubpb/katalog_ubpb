10a88831f2edbe2bbdac7ea1f4844ebbbda64fbb
Michael Sievers <michael_sievers@web.de>
2015-11-11 16:57:36 +0100

    Refactored user/loans#index
    - service now returns loans with filled common attributes
    - loans are rendered async in order to handle many loans gracefully

--- app/controllers/users/loans_controller.rb
+++ app/controllers/users/loans_controller.rb
@@ -5,31 +5,20 @@ class Users::LoansController < UsersController
   end, only: :index
 
   def index
-    ils_adapter = KatalogUbpb.config.ils_adapter.instance
-    search_engine_adapter = KatalogUbpb.config.ils_adapter.scope.search_engine_adapter.instance
-    @scope = KatalogUbpb.config.ils_adapter.scope
+    if async_content_request?(:loans)
+      ils_adapter = KatalogUbpb.config.ils_adapter.instance
+      search_engine_adapter = KatalogUbpb.config.ils_adapter.scope.search_engine_adapter.instance
 
-    @loans = GetUserLoansService.call(
-      adapter: ils_adapter,
-      user: current_user
-    )
-
-    @search_result = SearchRecordsService.call(
-      adapter: search_engine_adapter,
-      search_request: @search_request = Skala::SearchRequest.new(
-        queries: [
-          {
-            type: "ordered_terms",
-            field: "id",
-            terms: @loans.map(&:record_id)
-          }
-        ],
-        size: @loans.length
+      @loans = GetUserLoansService.call(
+        ils_adapter: ils_adapter,
+        search_engine_adapter: search_engine_adapter,
+        user: current_user,
       )
-    )
 
-    @records_by_id = @search_result.hits.each_with_object({}) do |_hit, _hash|
-      _hash[_hit.fields["id"]] = _hit
+      @loans = @loans.sort_by(&:due_date)
+      @scope = current_scope
+    else
+      # regular index
     end
   end
 
--- app/services/get_user_loans_service.rb
+++ app/services/get_user_loans_service.rb
@@ -2,16 +2,39 @@ class GetUserLoansService < Servizio::Service
   include CachingService
   include UserRelatedService
 
-  attr_accessor :adapter
+  attr_accessor :ils_adapter
+  attr_accessor :search_engine_adapter
 
-  validates_presence_of :adapter
-  validates_presence_of :ilsuserid
+  validates_presence_of :ils_adapter
+  validates_presence_of :ils_user_id
 
   def call
-    cache(key: [self.class, ilsuserid]) do
-      adapter.get_user_loans(ilsuserid).try(:loans)
+    ils_adapter.get_user_loans(ilsuserid).try(:loans).tap do |_loans|
+      if _loans.present? && search_engine_adapter
+        search_result = SearchRecordsService.call(
+          adapter: search_engine_adapter,
+          search_request: Skala::SearchRequest.new(
+            queries: [
+              {
+                type: "ordered_terms", # TODO: no need for ordered_terms here, use unscored_terms
+                field: "ils_record_id",
+                terms: _loans.map(&:ils_record_id)
+              }
+            ],
+            size: _loans.length.to_i
+          )
+        )
+
+        _loans.each do |_loan|
+          if hit = search_result.hits.find { |_hit| _hit.fields["ils_record_id"] == _loan.ils_record_id }
+            _loan.search_engine_record_id = hit.id
+            _loan.signature = hit.fields["signature"]
+            _loan.title = hit.fields["title"]
+          end
+        end
+      end
     end
-  rescue
+  rescue Skala::Adapter::Error
     errors[:call] = :failed and return nil
   end
 end
