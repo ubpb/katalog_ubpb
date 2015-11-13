6bbfdc7c18325e4ba32d24606c1792026e960bda
Michael Sievers <michael_sievers@web.de>
2015-11-13 10:24:50 +0100

    Made former_loans load async

--- app/controllers/users/former_loans_controller.rb
+++ app/controllers/users/former_loans_controller.rb
@@ -1,37 +1,23 @@
 class Users::FormerLoansController < UsersController
-  MAX_FORMER_LOANS ||= 250
-
   before_action -> do
     add_breadcrumb name: "users#show", url: user_path
     add_breadcrumb
   end, only: :index
 
   def index
-    ils_adapter = KatalogUbpb.config.ils_adapter.instance
-    search_engine_adapter = KatalogUbpb.config.ils_adapter.scope.search_engine_adapter.instance
-    @scope = KatalogUbpb.config.ils_adapter.scope
+    if async_content_request?(:former_loans)
+      ils_adapter = KatalogUbpb.config.ils_adapter.instance
+      search_engine_adapter = KatalogUbpb.config.ils_adapter.scope.search_engine_adapter.instance
 
-    @former_loans = GetUserFormerLoansService.call(
-      adapter: ils_adapter,
-      user: current_user
-    ).try(:[], 0..MAX_FORMER_LOANS)
-
-    @search_result = SearchRecordsService.call(
-      adapter: search_engine_adapter,
-      search_request: @search_request = Skala::SearchRequest.new(
-        queries: [
-          {
-            type: "ordered_terms",
-            field: "id",
-            terms: @former_loans.map(&:ils_record_id)
-          }
-        ],
-        size: @former_loans.length
+      @former_loans = GetUserFormerLoansService.call(
+        ils_adapter: ils_adapter,
+        limit: @limit = 100,
+        search_engine_adapter: search_engine_adapter,
+        user: current_user,
       )
-    )
 
-    @records_by_id = @search_result.hits.each_with_object({}) do |_hit, _hash|
-      _hash[_hit.fields["id"]] = _hit
+      @former_loans = @former_loans.sort_by(&:due_date)
+      @scope = current_scope
     end
   end
 end
--- app/services/get_user_former_loans_service.rb
+++ app/services/get_user_former_loans_service.rb
@@ -1,14 +1,44 @@
 class GetUserFormerLoansService < Servizio::Service
+  include CachingService
   include UserRelatedService
 
-  attr_accessor :adapter
+  attr_accessor :ils_adapter
+  attr_accessor :limit
+  attr_accessor :search_engine_adapter
 
-  validates_presence_of :adapter
-  validates_presence_of :ilsuserid
+  alias_method :adapter,  :ils_adapter
+  alias_method :adapter=, :ils_adapter=
+
+  validates_presence_of :ils_adapter
+  validates_presence_of :ils_user_id
 
   def call
-    adapter.get_user_former_loans(ilsuserid).try(:former_loans)
-  rescue
+    ils_adapter.get_user_former_loans(ilsuserid).try(:former_loans).try(:[], 0..(limit || -1)).tap do |_former_loans|
+      if _former_loans.present? && search_engine_adapter
+        search_result = SearchRecordsService.call(
+          adapter: search_engine_adapter,
+          search_request: Skala::SearchRequest.new(
+            queries: [
+              {
+                type: "ordered_terms", # TODO: no need for ordered_terms here, use unscored_terms
+                field: "ils_record_id",
+                terms: _former_loans.map(&:ils_record_id)
+              }
+            ],
+            size: _former_loans.length.to_i
+          )
+        )
+
+        _former_loans.each do |_former_loan|
+          if hit = search_result.hits.find { |_hit| _hit.fields["ils_record_id"] == _former_loan.ils_record_id }
+            _former_loan.search_engine_record_id = hit.id
+            _former_loan.signature = hit.fields["signature"]
+            _former_loan.title = hit.fields["title"]
+          end
+        end
+      end
+    end
+  rescue Skala::Adapter::Error
     errors[:call] = :failed and return nil
   end
 end
