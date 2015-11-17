fd683e476df1ec1a75fc55570017ec1205179c24
René Sprotte <rene.sprotte@provideal.net>
2015-11-17 11:38:58 +0100

    Cleanup routes. Fixes #18.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
-  resources :closed_stack_orders, only: [:new, :create], path: 'cso'
-  resource  :homepage, only: [:show]
-  resources :records, only: [:show]
+  resource  :session, only: [:create, :destroy, :new]
   resources :searches, only: [:index, :show] # :show only for compatibility with former catalog
+  resources :records, only: [:show]
+  resources :closed_stack_orders, only: [:new, :create], path: "cso"
 
-  resource :session, only: [:create, :destroy, :new] do
-    resource :local, only: [:update]
-  end
-
-  resources :tickets, :only => [:new, :create]
-  resources :thumbnails, :only => :show
-
-  resource :user, only: [:show, :update] do
-    scope module: "users" do
+  resource :user, only: [:show] do
+    scope module: :users do
+      resource  :password, only: [:edit, :update]
       resource  :email_address, only: [:edit, :update]
       resources :fees, only: [:index]
       resources :former_loans, only: [:index]
@@ -26,11 +20,10 @@ Rails.application.routes.draw do
 
       resources :calendars, only: [:index]
 
-      resource :password, only: [:edit, :update]
       resources :watch_lists do
         delete :index, on: :collection, action: :destroy
 
-        scope module: "watch_lists" do
+        scope module: :watch_lists do
           resources :entries, only: [:create, :destroy] do
             delete :index, on: :collection, action: :destroy
           end
@@ -39,32 +32,32 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/semapp/info' => 'semapp#info', as: :semapp_info
-  get '/realtime_availability' => 'realtime_availability#check_availability'
-
-
+  #
+  # Some kickers
+  #
   get "/go/signature", to: redirect("http://www.ub.uni-paderborn.de/lernen_und_arbeiten/bestaende/medienaufstellung-systemstelle.shtml"), as: :go_signature
 
-
-  root :to => "homepage#show"
+  #
+  # Root path
+  #
+  root to: "homepage#show"
 
   #
-  # api
+  # API
   #
   namespace :api do
     namespace :v1 do
-      get 'user/calendar' => 'calendar#show'
-      resources :records, only: [:index, :show], :constraints => { :id => /.*/ } do
-        scope module: 'records' do
+      get "user/calendar" => "calendar#show"
+
+      resources :records, only: [:index, :show] do
+        scope module: "records" do
           resources :items, only: [:index]
         end
       end
 
-      resources :users, only: [:show, :update] do
+      resources :users, only: [] do
         scope module: "users" do
           resources :notes, only: [:create, :update, :destroy]
-          resource :password, only: [:update]
-
           resources :watch_lists do
             scope module: "watch_lists" do
               resources :entries, only: [:create, :destroy]
