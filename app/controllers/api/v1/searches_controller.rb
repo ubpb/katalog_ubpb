4686f7ec519b4ef3bd41c99cc9940765796776c0
René Sprotte <rene.sprotte@provideal.net>
2015-12-03 10:42:39 +0100

    Implement searches for API.

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::SearchesController < Api::V1::ApplicationController
+  def index
+    scope = current_scope
+    ids   = [*params[:id]]
+
+    search_request = search_request_from_params
+
+    if search_request.present?
+      search_result = SearchRecordsService.call(
+        adapter: current_scope.search_engine_adapter.instance,
+        facets: current_scope.facets,
+        search_request: search_request
+      )
+
+      render json: search_result.to_json
+    else
+      render json: []
+    end
+  end
+
+private
+
+  def search_request_from_params
+    if params[:search_request]
+      JSON.parse(params[:search_request]).try do |_deserialized_search_request|
+        Skala::Adapter::Search::Request.new(_deserialized_search_request)
+      end
+    end
+  rescue
+    raise MalformedSearchRequestError
+  end
+
+end
