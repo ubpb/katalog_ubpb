f59a8e7a042961ff837501ecfdef96699bf2832e
Michael Sievers <michael_sievers@web.de>
2015-12-03 12:03:42 +0100

    Changed param name for get_records query

--- lib/katalog_ubpb/ubpb_elasticsearch_adapter/get_records.rb
+++ lib/katalog_ubpb/ubpb_elasticsearch_adapter/get_records.rb
@@ -11,7 +11,7 @@ class KatalogUbpb::UbpbElasticsearchAdapter::GetRecords < Skala::ElasticsearchAd
   # We implement get_records via search for the ubpb, so we can get records by their source id.
   # The original get needs the elasticsearch record id, which does not need to be the same.
   def call(record_ids)
-    search_result = adapter.search({ queries: [{ type: "unscored_terms", field: "id", query: record_ids }] })
+    search_result = adapter.search({ queries: [{ type: "unscored_terms", field: "id", terms: record_ids }] })
     self.class::Result.new({records: search_result.hits}).tap do |_get_records_result|
       _get_records_result.source = search_result.source
       _get_records_result.each { |_element| _element.found = true }
