ce90a915d757ff0c9fb3b276233d0dd00f76c4c6
René Sprotte <rene.sprotte@provideal.net>
2015-12-03 10:42:39 +0100

    Implement searches for API.

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "user/calendar" => "calendar#show"
 
+      resources :searches, only: [:index], path: ":scope/searches"
+
       resources :records, only: [:index, :show], path: ":scope/records" do
         scope module: "records" do
           resources :items, only: [:index]
