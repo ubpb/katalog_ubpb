5d633b44a73c0dbd72365684e3920010b50e9d84
René Sprotte <rene.sprotte@provideal.net>
2015-12-03 10:34:31 +0100

    Re-implement records#index and records#show in API.

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,49 +1,24 @@
 class Api::V1::RecordsController < Api::V1::ApplicationController
   def index
     scope = current_scope
-    binding.pry
+    ids   = [*params[:id]]
+
+    get_records_result = GetRecordsService.call(
+      adapter: scope.search_engine_adapter.instance,
+      ids: ids
+    )
+
+    render json: get_records_result.to_json
   end
 
   def show
-    scope = KatalogUbpb.config.find_search_scope(params[:scope])
+    scope = current_scope
 
-    get_record = Skala::GetRecord.new({
-      id: params[:id],
-      search_engine_adapter: scope.search_engine_adapter
-    })
+    get_records_result = GetRecordsService.call(
+      adapter: scope.search_engine_adapter.instance,
+      ids: params[:id]
+    ).first
 
-    if can?(:call, operation = get_record)
-      if operation.valid?
-        if operation.call!.succeeded?
-          record = operation.result
-
-          respond_to do |format|
-            format.bibtex do
-              if params[:download].try(:downcase) == "true"
-                send_data(record.to_bibtex, filename: "#{record.id}.bib")
-              else
-                render text: record.to_bibtex
-              end
-            end
-
-            format.json do
-              json = params[:pretty] == "true" ? JSON.pretty_generate(record.as_json) : record.to_json
-
-              if params[:download].try(:downcase) == "true"
-                send_data(json, filename: "#{record.id}.json")
-              else
-                render :json => json
-              end
-            end
-          end
-        else
-          head operation.errors[:http_status].first || :internal_server_error
-        end
-      else
-        head :bad_request
-      end
-    else
-      head :unauthorized
-    end
+    render json: get_records_result.to_json
   end
 end
