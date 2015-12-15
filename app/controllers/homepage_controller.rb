b3c567b6dcc65c76c494d17532d894017176ffd9
René Sprotte <rene.sprotte@provideal.net>
2015-12-15 17:00:38 +0100

    Tweek homepage.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,5 +4,7 @@ class HomepageController < ApplicationController
     @search_request = Skala::Adapter::Search::Request.new(
       queries: [{ type: "simple_query_string", fields: [@scope.searchable_fields.first] }]
     )
+
+    @news = FeedParser.parse_rss_feed("http://blogs.uni-paderborn.de/ub-katalog?feed=rss")
   end
 end
