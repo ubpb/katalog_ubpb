2ae3dff18a05c4116018608930fde56b275ca955
Michael Sievers <michael_sievers@web.de>
2015-12-18 15:22:24 +0100

    Added search_request.queries sanitizing

--- app/services/search_records_service.rb
+++ app/services/search_records_service.rb
@@ -4,28 +4,30 @@ class SearchRecordsService < Servizio::Service
   attr_accessor :adapter
   attr_accessor :facets
   attr_accessor :options
+  attr_accessor :sanitize_search_request
   attr_accessor :search_request
-  attr_accessor :request
 
   validates_presence_of :adapter
   validates_presence_of :search_request
 
-  def faceted_search_request
-    if facets
-      if search_request.is_a?(Hash)
-        search_request.deep_dup.merge(facets: facets)
-      elsif search_request.respond_to?(:facets=)
-        search_request.deep_dup.tap do |_dupped_search_request|
-          _dupped_search_request.facets = facets
-        end
-      end
-    else
-      search_request
-    end
+  def facets?
+    facets.present?
+  end
+
+  def sanitize_search_request
+    [true, false].include?(@sanitize_search_request) ? @sanitize_search_request : true
+  end
+
+  def sanitize_search_request?
+    sanitize_search_request == true
   end
 
   def call
-    result = adapter.search(faceted_search_request, options)
+    dupped_search_request = search_request.deep_dup
+    add_facets!(dupped_search_request) if facets?
+    sanitize!(dupped_search_request) if sanitize_search_request?
+
+    result = adapter.search(dupped_search_request, options)
 
     # Order facets the way they have been configured/requested
     # Also filter facets that are not configured
@@ -38,5 +40,39 @@ class SearchRecordsService < Servizio::Service
     end
 
     result
+  end
+
+  private
+
+  def add_facets!(search_request)
+    search_request.tap do |_search_request|
+      if _search_request.is_a?(Hash)
+        _search_request.merge!(facets: facets)
+      elsif _search_request.respond_to?(:facets=)
+        _search_request.facets = facets
+      end
+    end
+  end
+
+  def escape(string, *characters)
+    characters.inject(string) do |_string, _character|
+      _string.gsub(_character, "\\#{_character}")
+    end
+  end
+
+  def sanitize!(search_request)
+    search_request.tap do |_search_request|
+      characters_blacklist = "[", "]"
+
+      if _search_request.is_a?(Hash)
+        _search_request[:queries].each do |_query|
+          _query[:query] = escape(_query[:query], *characters_blacklist)
+        end
+      elsif _search_request.respond_to?(:queries)
+        _search_request.queries.each do |_query|
+          _query.query = escape(_query.query, *characters_blacklist)
+        end
+      end
+    end
   end
 end
