c35c871d30765916564636d5ebadf78ea8ef3138
Michael Sievers <michael_sievers@web.de>
2016-01-14 14:51:30 +0100

    Added inter library loans

---
+++ app/services/get_user_inter_library_loans_service.rb
@@ -0,0 +1,20 @@
+class GetUserInterLibraryLoansService < Servizio::Service
+  include AdapterRelatedService
+  include CachingService
+  include InstrumentedService
+  include UserRelatedService
+
+  attr_accessor :ils_adapter
+
+  alias_method :adapter,  :ils_adapter
+  alias_method :adapter=, :ils_adapter=
+
+  validates_presence_of :ils_adapter
+  validates_presence_of :ils_user_id
+
+  def call
+    ils_adapter.get_user_inter_library_loans(ils_user_id)
+  rescue Skala::Adapter::Error
+    errors[:call] = :failed and return nil
+  end
+end
