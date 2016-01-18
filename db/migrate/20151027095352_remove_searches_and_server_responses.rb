f184961f64223c0ac892da9d4b5bbb894f2ebe7b
René Sprotte <rene.sprotte@provideal.net>
2016-01-18 16:14:03 +0100

    Remove migration. We won't delete searches and server_responses for now.

--- db/migrate/20151027095352_remove_searches_and_server_responses.rb
+++
@@ -1,6 +0,0 @@
-class RemoveSearchesAndServerResponses < ActiveRecord::Migration
-  def up
-    drop_table :searches if table_exists? :searches
-    drop_table :server_responses if table_exists? :server_responses
-  end
-end
