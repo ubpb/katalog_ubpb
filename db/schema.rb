321f4f5224be6b8f9c991b46d9b5f8a161f9f79e
René Sprotte <rene.sprotte@provideal.net>
2016-01-18 16:14:15 +0100

    Bump.

--- db/schema.rb
+++ db/schema.rb
@@ -34,18 +34,14 @@ ActiveRecord::Schema.define(version: 20160111105435) do
   add_index "notes", ["record_id"], name: "index_notes_on_record_id", using: :btree
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
 
-  create_table "searches", force: :cascade do |t|
-    t.integer  "user_id",            limit: 4
-    t.text     "request",            limit: 4294967295
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.integer  "server_response_id", limit: 4
-    t.text     "hashed_id",          limit: 4294967295
-    t.text     "query",              limit: 4294967295
+  create_table "server_responses", force: :cascade do |t|
+    t.text     "request_hash", limit: 4294967295
+    t.text     "data",         limit: 4294967295
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
-  add_index "searches", ["hashed_id"], name: "index_searches_on_hashed_id", length: {"hashed_id"=>200}, using: :btree
-  add_index "searches", ["server_response_id"], name: "index_searches_on_server_response_id", using: :btree
+  add_index "server_responses", ["request_hash"], name: "index_server_responses_on_request_hash", length: {"request_hash"=>200}, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",              limit: 255
