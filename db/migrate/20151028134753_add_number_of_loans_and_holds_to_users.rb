9517f874d32335a41eed2abdcfbf74c1cfee83a8
Michael Sievers <michael_sievers@web.de>
2016-01-19 13:13:40 +0100

    Fixed indent in migration

--- db/migrate/20151028134753_add_number_of_loans_and_holds_to_users.rb
+++ db/migrate/20151028134753_add_number_of_loans_and_holds_to_users.rb
@@ -1,6 +1,6 @@
 class AddNumberOfLoansAndHoldsToUsers < ActiveRecord::Migration
   def up
-     add_column :users, :number_of_hold_requests, :integer
-     add_column :users, :number_of_loans,         :integer
+    add_column :users, :number_of_hold_requests, :integer
+    add_column :users, :number_of_loans,         :integer
   end
 end
