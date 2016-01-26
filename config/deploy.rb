638f1c49df5cd5ff96c36a3f2e0ebd870eafd279
René Sprotte <rene.sprotte@provideal.net>
2016-01-26 08:38:43 +0100

    Add newrelic.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch,      "master"
 set :log_level,   :debug
 
 set :linked_files, fetch(:linked_files, []).push(
-  "config/database.yml", "config/secrets.yml", "config/config.yml"
+  "config/database.yml", "config/secrets.yml", "config/config.yml", "config/newrelic.yml"
 )
 set :linked_dirs, fetch(:linked_dirs, []).push(
   "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle",
