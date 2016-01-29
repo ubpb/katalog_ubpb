b3da796f526649c612018ebf418c2df21e671e90
Michael Sievers <michael_sievers@web.de>
2016-01-29 10:03:33 +0100

    Added infrastructure and mappings for changing search requests
    - ubpb/issues#28

---
+++ app/controllers/compatibility/records_controller.rb
@@ -0,0 +1,17 @@
+class Compatibility::RecordsController < ApplicationController
+  skip_before_action :add_breadcrumb, :only => [:show]
+
+  def show
+    record_id = params[:id]
+    flash[:notice] = t(".please_update_url")
+
+    if record_id.upcase.starts_with?("PAD_ALEPH")
+      # Local record
+      redirect_to record_path(record_id.gsub(/PAD_ALEPH/i, ""), scope: "local")
+    else
+      # Primo central record
+      redirect_to record_path(record_id, scope: "primo_central")
+    end
+  end
+
+end
