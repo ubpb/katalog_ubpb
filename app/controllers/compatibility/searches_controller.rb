54da8aefceec953b88d7c08476caad8656535ce7
Michael Sievers <michael_sievers@web.de>
2016-02-01 15:30:53 +0100

    Final changes to be able to close issue 28
    - ubpb/issues#28

--- app/controllers/compatibility/searches_controller.rb
+++ app/controllers/compatibility/searches_controller.rb
@@ -12,10 +12,9 @@ class Compatibility::SearchesController < ApplicationController
   end
 
   def show
-    logger.info "Former (saved) search called, ip: #{request.ip || 'nil'}, referer: #{request.referer || 'nil'}, params: #{params}"
-    flash[:notice] = t("v1_searches.redirected_from_old_permalink")
-
-    if (search = V1Search.find_by_hashed_id(params[:id])) && former_catalog_search?(query = search.query)
+    if (search = Compatibility::Search.find_by_hashed_id(params[:id])) && former_catalog_search?(query = search.query)
+      log_former_catalog_search!
+      flash[:notice] = t(".please_update_url")
       redirect_to_translated_searches_path(query)
     else
       redirect_to root_path
@@ -29,7 +28,7 @@ class Compatibility::SearchesController < ApplicationController
   end
 
   def log_former_catalog_search!
-    logger.info { "Former search request submitted, ip: #{request.ip || 'nil'}, referer: #{request.referer || 'nil'}, params: #{params}" }
+    logger.info { "Former search request submitted, referer: #{request.referer || 'nil'}" }
   end
 
   def redirect_to_translated_searches_path(params = self.params)
