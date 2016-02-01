69651c7282ec354a1b680e884c3f9a530c1f2e98
Michael Sievers <michael_sievers@web.de>
2016-02-01 15:30:53 +0100

    Final changes to be able to close issue 28
    - ubpb/issues#28

--- app/models/compatibility/search.rb
+++ app/models/compatibility/search.rb
@@ -1,5 +1,5 @@
 # This is only a dummy in order to be able to access version 1.x searches
-class V1Search < ActiveRecord::Base
+class Compatibility::Search < ActiveRecord::Base
   self.table_name = "searches"
 
   belongs_to :user
