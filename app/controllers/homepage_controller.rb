d75974ebd8ed9c7151de8da1109e4f937e23ee46
René Sprotte <rene.sprotte@provideal.net>
2016-04-08 10:23:01 +0200

    Add a simple search option to the root controller.
    Now /?q=linux triggers a default search for "linux".

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,20 @@
 class HomepageController < ApplicationController
   def show
-    @scope = current_scope
-    @search_request = Skala::Adapter::Search::Request.new(
-      queries: [{ type: "query_string", fields: [@scope.searchable_fields.first] }]
-    )
+    if params[:q].present?
+      search_request = Skala::Adapter::Search::Request.new(
+        queries: [{ type: "query_string", query: params[:q]}]
+      )
 
-    @news = FeedParser.parse_rss_feed("https://blogs.uni-paderborn.de/ub-katalog?feed=rss")
+      return redirect_to(
+        searches_path(scope: current_scope, search_request: search_request)
+      )
+    else
+      @scope = current_scope
+      @search_request = Skala::Adapter::Search::Request.new(
+        queries: [{ type: "query_string", fields: [@scope.searchable_fields.first] }]
+      )
+
+      @news = FeedParser.parse_rss_feed("https://blogs.uni-paderborn.de/ub-katalog?feed=rss")
+    end
   end
 end
