60633ce2d8d91b20b927c4c9213ee7956ef29e93
René Sprotte <rene.sprotte@provideal.net>
2016-06-08 10:57:49 +0200

    Use locale files für password reset strings.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,7 +11,7 @@ class PasswordsController < ApplicationController
     @password_form = PasswordForm.new(password_params)
 
     if @password_form.valid? && ils_adapter.update_user(@user.ilsuserid, password: @password_form.password) && @user.clear_password_reset_token!
-      flash[:success] = "Sie haben Ihr Passwort erfolgreich zurückgesetzt. Sie können Sich jetzt mit Ihrem neuen Passwort anmelden."
+      flash[:success] = t(".flash_success")
       redirect_to(new_session_path)
     else
       render :edit
@@ -25,10 +25,10 @@ private
     @user = User.find_by(password_reset_token: @token)
 
     if @user.blank?
-      flash[:error] = "Ihr Passwort kann nicht zurückgesetzt werden. Der verwendete Link ist ungültig. Bitte starten Sie den Rücksetzvorgang erneut."
+      flash[:error] = t("passwords.flash_no_user_error")
       return redirect_to(new_session_path)
     elsif @user.password_reset_token_created_at + 24.hours <= Time.zone.now
-      flash[:error] = "Ihr Passwort kann nicht zurückgesetzt werden. Der verwendete Link ist älter als 24 Stunden. Bitte starten Sie den Rücksetzvorgang erneut."
+      flash[:error] = t("passwords.flash_token_to_old_error")
       return redirect_to(new_session_path)
     end
   end
