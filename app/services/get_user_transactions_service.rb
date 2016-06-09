edb7a5e8422733ae388d34bcd80a2eb0547fbd14
René Sprotte <rene.sprotte@provideal.net>
2016-06-09 10:46:12 +0200

    Fix ubpb/issues#81.

--- app/services/get_user_transactions_service.rb
+++ app/services/get_user_transactions_service.rb
@@ -18,7 +18,8 @@ class GetUserTransactionsService < Servizio::Service
     cache_key = "#{self.class.name}+#{ilsuserid}"
     cache(key: cache_key) do
       ils_adapter_result = ils_adapter.get_user_transactions(ilsuserid)
-      update_records!(ils_adapter_result, search_engine_adapter)
+      # Do not update records! See issue #81.
+      #update_records!(ils_adapter_result, search_engine_adapter)
       strip_source!(ils_adapter_result)
     end
   rescue Skala::Adapter::Error
