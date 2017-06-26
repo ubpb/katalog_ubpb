c7082df3757f8130b5e31bf1e8d84f0c81156939
René Sprotte <renspr@gmail.com>
2017-06-26 16:21:06 +0200

    Change deploy path for production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server "ubkatalog1", user: "ubpb", roles: %w{app web db}
 server "ubkatalog2", user: "ubpb", roles: %w{app web}
 set :branch, "production"
-set :deploy_to, "/ubpb/katalog-production"
+set :deploy_to, "/ubpb/katalog"
