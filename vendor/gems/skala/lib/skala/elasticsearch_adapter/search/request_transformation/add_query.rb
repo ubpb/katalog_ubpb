42d09154226eb14e41f1af59800c29c45d139fb3
René Sprotte <renspr@gmail.com>
2018-06-20 16:24:11 +0200

    Implement different stop word handling szenarios for testing.

--- vendor/gems/skala/lib/skala/elasticsearch_adapter/search/request_transformation/add_query.rb
+++ vendor/gems/skala/lib/skala/elasticsearch_adapter/search/request_transformation/add_query.rb
@@ -12,27 +12,45 @@ class Skala::ElasticsearchAdapter::Search::RequestTransformation::
       target["query"]["bool"]["must_not"] ||= []
       target["query"]["bool"]["should"] ||= []
 
-      if elasticsearch_query = elasticsearch_query_factory(_query)
-        container = _query.exclude ? target["query"]["bool"]["must_not"] : target["query"]["bool"]["must"] 
-        container << elasticsearch_query
+      if (_query.type.to_sym == :query_string || _query.type.to_sym == :simple_query_string) && (swm_match = _query.query.match(/STOP_WORD_MODE\\=(\d)/) )
+        stop_word_mode = swm_match[1]
+        _query.query = _query.query.gsub(/STOP_WORD_MODE\\=(\d)/, "")
       end
 
-      if _query.type.to_sym == :query_string
-        target["query"]["bool"]["should"] << query_from_query_string_query(_query, analyzer: "default_no_stop_words_search")
-      end
+      case stop_word_mode
+      when "1" # Do a normal search without filtering stop words
+        if elasticsearch_query = elasticsearch_query_factory(_query, analyzer: "default_with_stop_words_search")
+          container = _query.exclude ? target["query"]["bool"]["must_not"] : target["query"]["bool"]["must"] 
+          container << elasticsearch_query
+        end
+      when "2" # Search by ignoring stop words, but rank using stop words
+        if elasticsearch_query = elasticsearch_query_factory(_query)
+          container = _query.exclude ? target["query"]["bool"]["must_not"] : target["query"]["bool"]["must"] 
+          container << elasticsearch_query
+        end
 
-      if _query.type.to_sym == :simple_query_string
-        target["query"]["bool"]["should"] << query_from_simple_query_string_query(_query, analyzer: "default_no_stop_words_search")
+        if _query.type.to_sym == :query_string
+          target["query"]["bool"]["should"] << query_from_query_string_query(_query, analyzer: "default_with_stop_words_search")
+        end
+
+        if _query.type.to_sym == :simple_query_string
+          target["query"]["bool"]["should"] << query_from_simple_query_string_query(_query, analyzer: "default_with_stop_words_search")
+        end
+      else
+        if elasticsearch_query = elasticsearch_query_factory(_query)
+          container = _query.exclude ? target["query"]["bool"]["must_not"] : target["query"]["bool"]["must"] 
+          container << elasticsearch_query
+        end
       end
     end
   end
 
   private
 
-  def elasticsearch_query_factory(query)
+  def elasticsearch_query_factory(query, analyzer: "default_search")
     case query.type.to_sym
-    when :query_string        then query_from_query_string_query(query)
-    when :simple_query_string then query_from_simple_query_string_query(query)
+    when :query_string        then query_from_query_string_query(query, analyzer: analyzer)
+    when :simple_query_string then query_from_simple_query_string_query(query, analyzer: analyzer)
     when :match               then query_from_match_query(query)
     when :ordered_terms       then query_from_ordered_terms_query(query)
     when :range               then query_from_range_query(query)
