b3350e2053700323c69e0cea2abf3ff07295d90d
René Sprotte <renspr@gmail.com>
2019-04-24 14:01:36 +0200

    Set production deploy for new cluster.

--- config/deploy/new-cluster.rb
+++
@@ -1,4 +0,0 @@
-server "ubpegasus18", user: "ubpb", roles: %w{app web db}
-server "ubperseus18", user: "ubpb", roles: %w{app web}
-set :branch, "es6"
-set :deploy_to, "/ubpb/katalog"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "ubkatalog1", user: "ubpb", roles: %w{app web db}
-server "ubkatalog2", user: "ubpb", roles: %w{app web}
+server "ubpegasus18", user: "ubpb", roles: %w{app web db}
+server "ubperseus18", user: "ubpb", roles: %w{app web}
 set :branch, "production"
 set :deploy_to, "/ubpb/katalog"
