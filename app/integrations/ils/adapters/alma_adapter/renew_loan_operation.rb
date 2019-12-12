1d035c84c13f14a5ec925a18a2998e2f4a2db153
René Sprotte <renspr@gmail.com>
2019-12-12 09:45:54 +0100

    Include message in result.

--- app/integrations/ils/adapters/alma_adapter/renew_loan_operation.rb
+++ app/integrations/ils/adapters/alma_adapter/renew_loan_operation.rb
@@ -3,13 +3,13 @@ module Ils::Adapters
     class RenewLoanOperation < Operation
 
       def call(user_id, loan_id)
-        adapter.api.post("users/#{user_id}/loans/#{loan_id}", params: {
+        response = adapter.api.post("users/#{user_id}/loans/#{loan_id}", params: {
           op: "renew"
         })
 
-        Ils::RenewLoanResult.new(loan_id: loan_id, success: true)
+        Ils::RenewLoanResult.new(loan_id: loan_id, success: true, message: response.dig("last_renew_status", "desc"))
       rescue AlmaApi::Error => e
-        Ils::RenewLoanResult.new(loan_id: loan_id, success: false)
+        Ils::RenewLoanResult.new(loan_id: loan_id, success: false, message: e.message)
       end
 
     end
--- app/integrations/ils/renew_loan_result.rb
+++ app/integrations/ils/renew_loan_result.rb
@@ -2,5 +2,6 @@ class Ils
   class RenewLoanResult < BaseStruct
     attribute :loan_id, Ils::Types::String
     attribute :success, Ils::Types::Bool.default(false)
+    attribute :message, Ils::Types::String.optional
   end
 end
