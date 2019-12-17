4b39737f076dc7e6569bf0fe89fdf6a5ce4c6d87
René Sprotte <renspr@gmail.com>
2019-12-17 15:31:37 +0100

    Cleanup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,17 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate!
+  before_action { add_breadcrumb name: "users#show", url: user_path }
+
+  layout "users"
+
   rescue_from NotAuthorizedError do
     flash[:error] = t(".not_authorized_error")
     redirect_to user_path
   end
 
-  before_action :authenticate!, except: [:events]
-  before_action { add_breadcrumb name: "users#show", url: user_path }
-
-  layout "user"
-
   def show
     @user = current_user
-  end
-
-  # Make sure some "before actions" are run AFTER the before_actions
-  # from the child classes but before render.
-  def render(*args)
-    cash_balance
-    super
-  end
-
-private
-
-  def cash_balance
-    @cash_balance = transactions.cash_balance
-  end
-
-  def transactions(from_cache: true)
-    ils_adapter = KatalogUbpb.config.ils_adapter.instance
-    search_engine_adapter = KatalogUbpb.config.ils_adapter.scope.search_engine_adapter.instance
-
-    GetUserTransactionsService.call(
-      ils_adapter: ils_adapter,
-      search_engine_adapter: search_engine_adapter,
-      user: current_user,
-      from_cache: from_cache,
-      max_cache_age: 2.hours
-    )
   end
 
 end
