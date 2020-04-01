2d802d2e2041e7bbdd864db71d56e776f22174de
René Sprotte <renspr@gmail.com>
2020-04-01 12:02:08 +0200

    Add cap task to enable and disable maintenance mode.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,3 +17,25 @@ set :rvm_type,         :user
 set :rvm_ruby_version, IO.read(".ruby-version").strip
 
 set :rails_env, "production"
+
+namespace :app do
+  namespace :maintenance do
+    desc 'Activate maintenance mode'
+    task :on do
+      on roles(:web), in: :parallel do |host|
+        within release_path do
+          execute :touch, "public/MAINTENANCE_ON"
+        end
+      end
+    end
+
+    desc 'Deactivate maintenance mode'
+    task :off do
+      on roles(:web), in: :parallel do |host|
+        within release_path do
+          execute :rm, "public/MAINTENANCE_ON"
+        end
+      end
+    end
+  end
+end
