80c33f4b3250c8766d6f55eab4b8dfd38787cdf2
René Sprotte <renspr@gmail.com>
2020-08-05 11:22:06 +0200

    Handle cases where journals have a mono signature and location.

--- app/helpers/journal_helper.rb
+++ app/helpers/journal_helper.rb
@@ -55,4 +55,42 @@ module JournalHelper
     journal_stocks.try(:any?) { |journal_stock| journal_stock["signature"]&.starts_with?("P00") } || false
   end
 
+  def has_any_mono_signature?(journal_stocks)
+    journal_stocks.try(:any?) { |journal_stock| has_mono_signature?(journal_stock) }
+  end
+
+  def has_mono_signature?(journal_stock)
+    journal_stock["signature"]&.match(/\AP\d\d[A-Z]{3,4}/)
+  end
+
+  def mono_signature_from_journal_stock_signature(signature)
+    signature.try(:[], /[A-Z]{3,4}.*/)
+  end
+
+  def collection_code_from_journal_stock_signature(signature)
+    signature&.match(/\AP(\d\d).*/).try(:[], 1)
+  end
+
+  def location_for_journal_stock_with_mono_signature(journal_stock)
+    collection_code = collection_code_from_journal_stock_signature(journal_stock["signature"])
+    notation = mono_signature_from_journal_stock_signature(journal_stock["signature"]).try(:[], /\A[A-Z]{3,4}/)
+
+    if collection_code && notation
+      LOCATION_LOOKUP_TABLE.find do |_row|
+        systemstellen_range = _row[:systemstellen]
+        standortkennziffern = _row[:standortkennziffern]
+
+        if systemstellen_range.present? && systemstellen_range.first.present? && systemstellen_range.last.present? && standortkennziffern.present?
+          # Expand systemstellen and notation to 4 chars to make ruby range include? work in this case.
+          justified_systemstellen_range = (systemstellen_range.first.ljust(4, "A") .. systemstellen_range.last.ljust( 4, "A"))
+          justified_notation = notation.ljust(4, "A")
+
+          standortkennziffern.include?(collection_code) && justified_systemstellen_range.include?(justified_notation)
+        end
+      end.try do |_row|
+        _row[:location]
+      end
+    end
+  end
+
 end
