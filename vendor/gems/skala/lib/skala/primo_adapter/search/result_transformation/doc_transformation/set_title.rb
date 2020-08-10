179c80a5d04e8d6378d1a708746dfd119b9c223a
René Sprotte <renspr@gmail.com>
2020-08-10 13:22:41 +0200

    Clean tags from title for some cases discovered with CDI.

--- vendor/gems/skala/lib/skala/primo_adapter/search/result_transformation/doc_transformation/set_title.rb
+++ vendor/gems/skala/lib/skala/primo_adapter/search/result_transformation/doc_transformation/set_title.rb
@@ -5,6 +5,7 @@ class Skala::PrimoAdapter::Search::ResultTransformation::DocTransformation::
   SetTitle < Transformator::Transformation::Step
 
   def call
-    target.record.title = transformation.read_source_values(".//display/title").first
+    title = transformation.read_source_values(".//display/title").first
+    target.record.title =  ActionView::Base.full_sanitizer.sanitize(title, tags: %w(span))
   end
 end
