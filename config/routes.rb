ba744ee9cbef6d06eac4b816b283cfd43ee6c072
René Sprotte <renspr@gmail.com>
2021-01-11 17:52:29 +0100

    typo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   get "/:scope/searches",    to: "searches#index", as: :searches
   get "/:scope/records/:id", to: "records#show", as: :record, constraints: { id: /.+/ }
